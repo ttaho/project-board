7996c012d203cbfc2cb7cc76ea867f135cb99765
Taeho <ice_98@naver.com>
2023-02-23 22:42:59 +0900

    #21 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.

---
+++ src/main/java/com/fastcampus/projectboard/controller/ArticleController.java
@@ -0,0 +1,15 @@
+package com.fastcampus.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+/*/articles
+/articles/{article-id}
+/articles/search
+/articles/search-hashtag
+*/
+
+
+@RequestMapping("/artiles")
+@Controller
+public class ArticleController {
+}
